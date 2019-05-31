fd093f208e632fd6934ee0b80c6de8245a709082
kk <fkfkbill@gmail.com>
2019-05-31 10:43:12 +0800

    f

--- restful_api/views/report.py
+++ restful_api/views/report.py
@@ -1,5 +1,7 @@
 # Author: kk.Fang(fkfkbill@gmail.com)
 
+from collections import defaultdict
+
 from schema import Optional, Schema, And
 
 from utils.schema_utils import *
@@ -34,22 +36,60 @@ class OnlineReportTaskListHandler(AuthReq):
         if date_end:
             job_q = job_q.filter(create_time__lte=date_end)
         jobs_to_ret, p = self.paginate(job_q, **p)
-        with make_session() as session:
-            cmdb_dict = {i.cmdb_id: i.to_dict() for i in session.query(CMDB).all()}
-            ret = []
-            for j in jobs_to_ret:
-                ret.append({
-                    "cmdb": cmdb_dict[j.cmdb_id],
-                    **j.to_dict()
-                })
-            self.resp(ret, **p)
+        ret = []
+        for j in jobs_to_ret:
+            ret.append({
+                **j.to_dict()
+            })
+        self.resp(ret, **p)
 
 
 class OnlineReportTaskHandler(AuthReq):
 
+    def calc_rules_and_score_in_one_result(self, session, result) -> tuple:
+        # TODO 需要确定这里查看的风险规则还是全部规则
+        rule_dict = rule_utils.get_rules_dict()
+        score_sum = 0
+        rule_name_to_detail = defaultdict(lambda: {
+            "violated_num": 0,
+            "rule": {},
+            "deduction": 0.0,
+            "weighted_deduction": 0.0
+        })
+        for k, rule_object in rule_dict.items():
+            db_type, db_model, rule_name = k
+            rule_result = getattr(result, rule_name, None)
+            if rule_result:
+
+                score_sum += rule_result["scores"]
+                rule_name_to_detail[rule_name]["rule"] = rule_object.to_dict(
+                    iter_if=lambda key, v: key in ("rule_name", "rule_desc"))
+                rule_name_to_detail[rule_name]["violated_num"] += 1
+                rule_name_to_detail[rule_name]["deduction"] += 1
+
+        return ()
+
     def get(self):
         """在线查看某个报告"""
-        self.resp()
+        params = self.get_query_args(Schema({
+            "job_id": scm_unempty_str,
+            "rule_type": scm_one_of_choices(rule_utils.ALL_RULE_TYPE)
+        }))
+        job_id = params.pop("job_id")
+        rule_type = params.pop("rule_type")
+        del params  # shouldn't use params anymore
+        with make_session() as session:
+            job = Job.objects(_id=job_id).first()
+            result = Results.objects(task_uuid=job_id).first()
+            cmdb = session.query(CMDB).filter_by(cmdb_id=result.cmdb_id)
+            rules_violated = []
+            self.resp({
+                "job_id": job_id,
+                "cmdb": cmdb.to_dict(),
+                "rules_violated": rules_violated,
+                "rule_type": rule_type,
+                "schema": job.desc["owner"]
+            })
 
 
 class OnlineReportRuleDetailHandler(AuthReq):
