fc6cbb914df9b174d6c677d70b64c42b05b8bcfd
kk <fkfkbill@gmail.com>
2019-06-04 19:23:18 +0800

    f

--- restful_api/views/task.py
+++ restful_api/views/task.py
@@ -5,6 +5,7 @@ from schema import Schema, Optional, And, Or
 from .base import AuthReq
 from models.oracle import *
 from utils.schema_utils import *
+from utils.datetime_utils import *
 
 
 class TaskHandler(AuthReq):
@@ -53,6 +54,9 @@ class TaskHandler(AuthReq):
             Optional("task_exec_frequency"): scm_int
         }))
         task_id = params.pop("task_id")
+        if "task_schedule_date" in params.keys():
+            params["task_schedule_date"] = arrow.get(params["task_schedule_date"]).\
+                strftime(COMMON_TIME_FORMAT)[:5]
         with make_session() as session:
             session.query(TaskManage).filter_by(task_id=task_id).update(params)
             self.resp_created()
