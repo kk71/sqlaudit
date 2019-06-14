d14b19c98da177682cca9e0b516f9c1ee19eb22e
kk <fkfkbill@gmail.com>
2019-06-14 13:57:26 +0800

    merged

--- restful_api/views/permisson.py
+++ restful_api/views/permisson.py
@@ -26,38 +26,38 @@ class SystemPermissionHandler(AuthReq):
         #     itesm, p = self.paginate(q, **params)
         #     ret = {
         #         privilege_id: privilege
-            # }
-            #
-            #
-            #
-            # q = session.query(Privilege).\
-            #     join(RolePrivilege, Privilege.privilege_id == RolePrivilege.privilege_id).\
-            #     filter(RolePrivilege.role_id == role_id)
-            # items, p = self.paginate(q, **params)
-            # ret = []
-            # for a_privege, role_privilege in items:
-            #     privilege_content = a_privege.to_dict()
-            #     privilege_content.update({"enabled": False})
-            #     if len(role_privilege):
-            #         privilege_content.update({"enabled": True})
-            #     ret.append(privilege_content)
-            # self.resp(ret, **p)
+        # }
+        #
+        #
+        #
+        # q = session.query(Privilege).\
+        #     join(RolePrivilege, Privilege.privilege_id == RolePrivilege.privilege_id).\
+        #     filter(RolePrivilege.role_id == role_id)
+        # items, p = self.paginate(q, **params)
+        # ret = []
+        # for a_privege, role_privilege in items:
+        #     privilege_content = a_privege.to_dict()
+        #     privilege_content.update({"enabled": False})
+        #     if len(role_privilege):
+        #         privilege_content.update({"enabled": True})
+        #     ret.append(privilege_content)
+        # self.resp(ret, **p)
 
-            # privileges = session.query(RolePrivilege).\
-            #     with_entities(RolePrivilege.role_id, RolePrivilege.privilege_id)
-            # privileges = [list(x) for x in privileges]
-            # res = defaultdict(list)
-            # for priv in privileges:
-            #     res[priv[0]].append(priv[1])
-            #
-            # roles = session.query(Role).with_entities(Role.role_id, Role.role_name)
-            # roles = dict([list(x) for x in roles])
-            #
-            # user_privileges = {roles[x]: res[x] for x in roles}
-            # sys_privileges = session.query(Privilege).\
-            #     with_entities(Privilege.privilege_id, Privilege.comments)
-            # sys_privileges = dict([list(x) for x in sys_privileges])
-            # self.resp({'user_privileges': user_privileges, 'sys_privileges': sys_privileges})
+        # privileges = session.query(RolePrivilege).\
+        #     with_entities(RolePrivilege.role_id, RolePrivilege.privilege_id)
+        # privileges = [list(x) for x in privileges]
+        # res = defaultdict(list)
+        # for priv in privileges:
+        #     res[priv[0]].append(priv[1])
+        #
+        # roles = session.query(Role).with_entities(Role.role_id, Role.role_name)
+        # roles = dict([list(x) for x in roles])
+        #
+        # user_privileges = {roles[x]: res[x] for x in roles}
+        # sys_privileges = session.query(Privilege).\
+        #     with_entities(Privilege.privilege_id, Privilege.comments)
+        # sys_privileges = dict([list(x) for x in sys_privileges])
+        # self.resp({'user_privileges': user_privileges, 'sys_privileges': sys_privileges})
 
     def patch(self):
         """部分更新系统权限"""
@@ -70,7 +70,7 @@ class SystemPermissionHandler(AuthReq):
 
             change_priv_ids = {int(x) for x in params["privilege_ids"].split(',')}
             priv_ids = session.query(RolePrivilege) \
-                .filter(RolePrivilege.role_id == params["role_id"]).\
+                .filter(RolePrivilege.role_id == params["role_id"]). \
                 with_entities(RolePrivilege.privilege_id)
             priv_ids = {priv_id[0] for priv_id in priv_ids}
             add_ids = change_priv_ids - priv_ids
@@ -95,101 +95,103 @@ class CMDBPermissionHandler(AuthReq):
             return self.resp_bad_req("No Authority")
 
         with make_session() as session:
-            perm_datas=session.query(DataPrivilege,CMDB,User).\
-                join(CMDB,DataPrivilege.cmdb_id==CMDB.cmdb_id).\
-                join(User,User.login_user==DataPrivilege.login_user).\
-                with_entities(CMDB.connect_name,CMDB.cmdb_id,
-                              DataPrivilege.schema_name,DataPrivilege.create_date,
-                              DataPrivilege.comments,User.user_name,User.login_user)
+            perm_datas = session.query(DataPrivilege, CMDB, User). \
+                join(CMDB, DataPrivilege.cmdb_id == CMDB.cmdb_id). \
+                join(User, User.login_user == DataPrivilege.login_user). \
+                with_entities(CMDB.connect_name, CMDB.cmdb_id,
+                              DataPrivilege.schema_name, DataPrivilege.create_date,
+                              DataPrivilege.comments, User.user_name, User.login_user)
             permisson_datas = []
             perm_datas = [list(x) for x in perm_datas]
             for perm_data in perm_datas:
                 permisson_datas.append({'connect_name': perm_data[0], 'cmdb_id': perm_data[1],
-                                    'schema_name': perm_data[2], 'create_date': dt_to_str(perm_data[3]),
-                                    'comments': perm_data[4], 'user_name': perm_data[5],
-                                    'login_user': perm_data[6]})
+                                        'schema_name': perm_data[2], 'create_date': dt_to_str(perm_data[3]),
+                                        'comments': perm_data[4], 'user_name': perm_data[5],
+                                        'login_user': perm_data[6]})
 
-            user=session.query(User).filter(User.login_user != "admin").\
-                with_entities(User.login_user,User.user_name)
+            user = session.query(User).filter(User.login_user != "admin"). \
+                with_entities(User.login_user, User.user_name)
             # users=[user.to_dict() for user in users]
             users = []
-            user=[list(x) for x in user]
+            user = [list(x) for x in user]
             for u in user:
-                users.append({'login_user':u[0],'user_name':u[1]})
+                users.append({'login_user': u[0], 'user_name': u[1]})
 
-            cmdb=session.query(CMDB).with_entities(CMDB.cmdb_id,CMDB.connect_name)
+            cmdb = session.query(CMDB).with_entities(CMDB.cmdb_id, CMDB.connect_name)
             # cmdbs=[cmdb.to_dict() for cmdb in cmdbs]
             cmdbs = []
-            cmdb =[list(x) for x in cmdb]
+            cmdb = [list(x) for x in cmdb]
             for c in cmdb:
-                cmdbs.append({'cmdb_id':c[0],'connect_name':c[1]})
+                cmdbs.append({'cmdb_id': c[0], 'connect_name': c[1]})
             self.resp({"permisson_datas": permisson_datas, "users": users, "cmdbs": cmdbs})
 
     def patch(self):
-        params=self.get_json_args(Schema({
-            "cmdb_id":scm_int,
-            "login_user":scm_unempty_str,
-            "schema_names":list
+        params = self.get_json_args(Schema({
+            "cmdb_id": scm_int,
+            "login_user": scm_unempty_str,
+            "schema_names": list
         }))
-        login_user,cmdb_id,schema_names=params.pop("login_user"),params.pop("cmdb_id"),params.pop("schema_names")
+        login_user, cmdb_id, schema_names = \
+            params.pop("login_user"), params.pop("cmdb_id"), params.pop("schema_names")
         del params
         with make_session() as session:
-            existing_schemas=session.query(DataPrivilege).filter(DataPrivilege.login_user==login_user,
-                DataPrivilege.cmdb_id==cmdb_id).\
+            existing_schemas = session.query(DataPrivilege).\
+                filter(DataPrivilege.login_user == login_user, DataPrivilege.cmdb_id == cmdb_id).\
                 with_entities(DataPrivilege.schema_name)
-            existing_schemas=[list(existing_schema)[0] for existing_schema in existing_schemas]
-            loading={f"{login_user}:{cmdb_id}:{schema}"for schema in schema_names}
-            existing={f"{login_user}:{cmdb_id}:{schema}" for schema in existing_schemas}
+            existing_schemas = [list(existing_schema)[0] for existing_schema in existing_schemas]
+            loading = {f"{login_user}:{cmdb_id}:{schema}" for schema in schema_names}
+            existing = {f"{login_user}:{cmdb_id}:{schema}" for schema in existing_schemas}
 
-            insert_schemas=[x.split(":")[2] for x in (loading-existing)]
-            delete_schemas=[x.split(":")[2] for x in (existing-loading)]
+            insert_schemas = [x.split(":")[2] for x in (loading - existing)]
+            delete_schemas = [x.split(":")[2] for x in (existing - loading)]
 
             if insert_schemas:
                 for schema in insert_schemas:
-                    config=DataPrivilege()
-                    config.login_user=login_user
-                    config.cmdb_id=cmdb_id
-                    config.schema_name=schema
-                    config.create_date=datetime.now()
+                    config = DataPrivilege()
+                    config.login_user = login_user
+                    config.cmdb_id = cmdb_id
+                    config.schema_name = schema
                     session.add(config)
             if delete_schemas:
-                session.query(DataPrivilege).filter(DataPrivilege.login_user==login_user,
-                                                    DataPrivilege.cmdb_id==cmdb_id,
-                                                    DataPrivilege.schema_name.in_(delete_schemas)).delete(synchronize_session='fetch')
+                session.query(DataPrivilege).filter(
+                    DataPrivilege.login_user == login_user,
+                    DataPrivilege.cmdb_id == cmdb_id,
+                    DataPrivilege.schema_name.in_(delete_schemas)).\
+                    delete(synchronize_session='fetch')
 
             self.resp_created(msg="分配权限成功")
 
     def post(self):
-        params=self.get_json_args(Schema({
-            "cmdb_id":scm_int,
-            "login_user":scm_unempty_str
+        params = self.get_json_args(Schema({
+            "cmdb_id": scm_int,
+            "login_user": scm_unempty_str
         }))
-        cmdb_id,login_user=params.pop("cmdb_id"),params.pop("login_user")
+        cmdb_id, login_user = params.pop("cmdb_id"), params.pop("login_user")
         del params
 
         from utils.cmdb_utils import get_cmdb_available_schemas
         with make_session() as session:
-            cmdb_objects=session.query(CMDB).filter(CMDB.cmdb_id==cmdb_id)
+            cmdb_objects = session.query(CMDB).filter(CMDB.cmdb_id == cmdb_id)
             for cmdb_object in cmdb_objects:
                 try:
-                    schemas=get_cmdb_available_schemas(cmdb_object)
+                    schemas = get_cmdb_available_schemas(cmdb_object)
                 except cx_Oracle.DatabaseError as err:
                     print(err)
                     return self.resp_bad_req(msg="无法连接到目标主机")
-            authed_schemas=session.query(DataPrivilege).filter(DataPrivilege.login_user==login_user,DataPrivilege.cmdb_id==cmdb_id).\
+            authed_schemas = session.query(DataPrivilege).\
+                filter(DataPrivilege.login_user == login_user,
+                       DataPrivilege.cmdb_id == cmdb_id).\
                 with_entities(DataPrivilege.schema_name)
-            authed_schemas=[list(authed_schema)[0].upper() for authed_schema in authed_schemas]
-            schemas={schema:1 if schema in authed_schemas else 0 for schema in schemas}
-            self.resp_created({'schemas':schemas})
+            authed_schemas = [list(authed_schema)[0].upper() for authed_schema in authed_schemas]
+            schemas = {schema: 1 if schema in authed_schemas else 0 for schema in schemas}
+            self.resp_created({'schemas': schemas})
 
     def delete(self):
-        params=self.get_json_args(Schema({
-            'cmdb_id':scm_int,
-            'login_user':scm_unempty_str,
-            'schema_name':scm_unempty_str
+        params = self.get_json_args(Schema({
+            'cmdb_id': scm_int,
+            'login_user': scm_unempty_str,
+            'schema_name': scm_unempty_str
         }))
         with make_session() as session:
             session.query(DataPrivilege).filter_by(**params).delete()
-
-            self.resp_created("删除权限成功")
-
+            self.resp_created(msg="删除权限成功")
