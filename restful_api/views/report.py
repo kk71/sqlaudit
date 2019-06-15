012f03a9944f7087ffa4fa7bc308e604d814d6f8
kk <fkfkbill@gmail.com>
2019-06-15 16:59:53 +0800

    f

--- restful_api/views/report.py
+++ restful_api/views/report.py
@@ -81,7 +81,7 @@ class OnlineReportRuleDetailHandler(AuthReq):
     def get_report_rule_detail(session, job_id: int, rule_name: str) -> dict:
         job = Job.objects(id=job_id).first()
         cmdb = session.query(CMDB).filter_by(cmdb_id=job.cmdb_id).first()
-        rule = Rule.objects(
+        rule = Rule.filter_enabled(
             rule_name=rule_name,
             rule_type=job.desc.rule_type,
             db_model=cmdb.db_model
@@ -248,7 +248,8 @@ class ExportReportXLSXHandler(AuthReq):
                 rule_name = rule_data[0]
                 rule_detail_data = OnlineReportRuleDetailHandler.\
                     get_report_rule_detail(session, job_id, rule_name)
-                rule_info = Rule.objects(rule_name=rule_name, db_model=cmdb.db_model).first()
+                rule_info = Rule.objects(rule_name=rule_name, db_model=cmdb.db_model,
+                                         db_type=const.DB_ORACLE).first()
                 solution = ''.join(rule_info['solution'])
                 rule_detail_datas = []
                 rule_detail_title = []
