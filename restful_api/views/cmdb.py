71b770f58665f7d585cc7970e36536722ddc218e
kk <fkfkbill@gmail.com>
2019-06-26 21:25:57 +0800

    why assertion?

--- restful_api/views/cmdb.py
+++ restful_api/views/cmdb.py
@@ -312,16 +312,13 @@ class CMDBHealthTrendHandler(AuthReq):
                 for dh in dh_q:
                     ret[dh.collect_date.date()][dh.database_name] = dh.health_score
                     fields.add(dh.database_name)
-            if len(ret) > 0:
-                base_lines = [i[0] for i in session.query(CMDB.baseline).
+            base_lines = [i[0] for i in session.query(CMDB.baseline).
                                 filter(CMDB.connect_name.in_(cmdb_connect_name_list)).
                                 order_by(CMDB.baseline)]
-                if not base_lines or base_lines[0] == 0:
-                    base_line = 80
-                else:
-                    base_line = base_lines[0]
+            if not base_lines or base_lines[0] == 0:
+                base_line = 80
             else:
-                assert 0
+                base_line = base_lines[0]
             ret = [{
                 "date": d_to_str(k),
                 "基线": base_line,
