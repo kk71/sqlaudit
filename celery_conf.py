9b9d2d23f2a59e5b95979d0fada85cb6bb45225c
kk <fkfkbill@gmail.com>
2019-07-11 14:59:38 +0800

    f

--- celery_conf.py
+++ celery_conf.py
@@ -11,7 +11,7 @@ platforms.C_FORCE_ROOT = True
 broker_url = settings.REDIS_BROKER
 result_backend = settings.REDIS_BACKEND
 
-timezone = 'Asia/Shanghai'
+# timezone = 'Asia/Shanghai'
 task_serializer = 'json'
 accept_content = ['pickle', 'json', 'msgpack', 'yaml']
 result_serializer = 'json'
