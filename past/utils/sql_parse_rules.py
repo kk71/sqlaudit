a17b8fcdcebda6eb8c6b4b70ede4f127feb4099c
kk <fkfkbill@gmail.com>
2019-08-16 16:28:19 +0800

    下面指定了noorder，但是审核依然判定用了order选项

--- past/utils/sql_parse_rules.py
+++ past/utils/sql_parse_rules.py
@@ -11,7 +11,7 @@ class ObjStaticRules:
         if not re.search(r"create\s+sequence", sql, re.I):
             return True, None
 
-        if 'order' in sql:
+        if ' order' in sql:
             return False, "使用了order的序列"
         if ' cache ' not in sql:
             return False, "没有显式指定cache并且序列的cache需要指定为2000或以上"
