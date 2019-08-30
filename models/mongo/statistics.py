45f9874f3cef02b4a66de481c760c86b82dbaa17
kk <fkfkbill@gmail.com>
2019-08-30 13:54:08 +0800

    fix

--- models/mongo/statistics.py
+++ models/mongo/statistics.py
@@ -346,7 +346,7 @@ class StatsCMDBLoginUser(BaseStatisticsDoc):
                         session=session,
                         cmdb_id=cmdb_id,
                         # schema_name=schema_name,
-                        date_range=(date_start.date(), date_end.date()),
+                        date_range=(date_start, date_end),
                         sqltext_stats=False
                     )
                     sql_by_sum = [
@@ -374,7 +374,7 @@ class StatsCMDBLoginUser(BaseStatisticsDoc):
                     doc.risk_rate = get_risk_rate(
                         session=session,
                         cmdb_id=cmdb_id,
-                        date_range=(date_start.date(), date_end.date())
+                        date_range=(date_start, date_end)
                     )
                     yield doc
 
