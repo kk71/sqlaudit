b5c28d84133d06b9b35417db3ce0e3cb689292f2
sqlaudit <sqlaudit@192.168.0.25>
2019-10-18 16:58:39 +0800

    statsdebug_and_riskrule

--- restful_api/views/online.py
+++ restful_api/views/online.py
@@ -17,6 +17,7 @@ from utils.datetime_utils import *
 from utils import rule_utils, sql_utils, object_utils, score_utils
 from models.oracle import *
 from models.mongo import *
+from models.mongo.statistics import StatsRiskObjectsRule,StatsRiskSqlRule
 
 
 class ObjectRiskListHandler(AuthReq):
@@ -50,6 +51,21 @@ class ObjectRiskListHandler(AuthReq):
                 object_utils.get_risk_object_list, session=session, **params)
             rst_this_page, p = self.paginate(rst, **p)
         self.resp(rst_this_page, **p)
+
+
+class ObjectRiskRuleHandler(AuthReq):
+
+    def get(self):
+        """风险对象外层规则,违反规则个数等"""
+        params=self.get_query_args(Schema({
+            Optional("schema_name", default=None): scm_str,
+            Optional("rule_desc", default=None): scm_dot_split_str,
+            Optional("severity", default=None): scm_dot_split_str,
+        }))
+
+        risk_obj_rule=StatsRiskObjectsRule.objects(**params)
+
+        self.resp(content=[x.to_dict() for x in risk_obj_rule])
 
 
 class ObjectRiskReportExportHandler(ObjectRiskListHandler):
@@ -175,6 +191,22 @@ class SQLRiskListHandler(PrivilegeReq):
                 return
             rst_this_page, p = self.paginate(rst, **p)
         self.resp(rst_this_page, **p)
+
+
+class SQLRiskRuleHandler(AuthReq):
+
+    def get(self):
+        """风险sql外层规则,违反规则个数等"""
+        params = self.get_query_args(Schema({
+            Optional("schema_name", default=None): scm_str,
+            Optional("rule_desc", default=None): scm_dot_split_str,
+            Optional("severity", default=None): scm_dot_split_str,
+        }))
+
+        risk_sql_rule=StatsRiskSqlRule.objects(**params)
+
+        self.resp(content=[x.to_dict() for x in risk_sql_rule])
+
 
 
 class SQLRiskReportExportHandler(SQLRiskListHandler):
--- utils/analyse_utils.py
+++ utils/analyse_utils.py
@@ -11,6 +11,8 @@ from models.mongo import *
 # 统计数据model
 # 注意：如果统计数据有先后依赖，需要在这里体现。
 STATS_MODELS = (
+    StatsRiskSqlRule,
+    StatsRiskObjectsRule,
     StatsNumDrillDown,
     StatsCMDBPhySize,
     StatsCMDBLoginUser,
