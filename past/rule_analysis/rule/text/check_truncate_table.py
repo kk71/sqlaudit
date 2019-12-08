80daff830c925db5cfae48b4ad625b844234fe4c
kk <fkfkbill@gmail.com>
2019-12-08 19:58:17 +0800

    feat add new 10 rules for tailong bank

---
+++ past/rule_analysis/rule/text/check_truncate_table.py
@@ -0,0 +1,15 @@
+# Author: kk.Fang(fkfkbill@gmail.com)
+
+import re
+
+from .utils import judge_if_ddl
+
+
+def execute_rule(sql, db_model=None, **kwargs):
+    if not judge_if_ddl(sql):
+        return False
+
+    if re.search('truncate\s+table', sql, re.I):
+        return True
+
+    return False
