e2a772c91fc9896b95317ff9dcf43caaf2c388d2
sqlaudit <sqlaudit@localhost.localdomain>
2019-12-17 01:05:20 +0800

    fix-位图索引和索引未定义表空间

--- past/rule_analysis/rule/text/check_index_not_define_table_space.py
+++ past/rule_analysis/rule/text/check_index_not_define_table_space.py
@@ -7,8 +7,8 @@ from .utils import judge_if_ddl
 
 def execute_rule(sql, db_model=None, **kwargs):
 
-    if not judge_if_ddl(sql):
-        return False
+    # if not judge_if_ddl(sql):
+    #     return False
 
     if re.search('create\s+index', sql, re.I) and 'tablespace' not in sql:
         #return "需要为索引指定表空间"
