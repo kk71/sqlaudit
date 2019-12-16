0f832672618e98750458d5e233bd47997c0d364e
sqlaudit <sqlaudit@192.168.186.134>
2019-12-16 17:25:18 +0800

    fix

--- past/rule_analysis/rule/text/check_create_tabel_not_define_table_space.py
+++ past/rule_analysis/rule/text/check_create_tabel_not_define_table_space.py
@@ -7,9 +7,6 @@ from .utils import judge_if_ddl
 
 def execute_rule(sql, db_model=None, **kwargs):
 
-    if not judge_if_ddl(sql):
-        return False
-
     if re.search('create\s+table', sql, re.I) and 'tablespace' not in sql:
         return True
     return False
--- past/rule_analysis/rule/text/check_index_online.py
+++ past/rule_analysis/rule/text/check_index_online.py
@@ -7,8 +7,6 @@ from .utils import judge_if_ddl
 
 def execute_rule(sql, db_model=None, **kwargs):
 
-    if not judge_if_ddl(sql):
-        return False
 
     if re.search(r'create\s+index\s+', sql, re.I):
         if not re.search(r"\s+online", sql, re.I):
