a9d812151fc5fb6f38c81b6bfdf55f026b969aa3
sqlaudit <sqlaudit@localhost.localdomain>
2019-12-17 01:20:35 +0800

    位图索引xxxd

--- past/rule_analysis/rule/text/check_bitmap_index.py
+++ past/rule_analysis/rule/text/check_bitmap_index.py
@@ -9,9 +9,8 @@ def execute_rule(sql, db_model=None, **kwargs):
     # if not judge_if_ddl(sql):
     #     return False
 
-    # if db_model == "OLTP" and re.search("create\s+bitmap\s+index", sql, re.I):
-    if re.search("create\s+bitmap\s+index", sql, re.I):
-        return True, "不建议创建位图索引"
+    if db_model == "OLTP" and re.search("create\s+bitmap\s+index", sql, re.I):
+        return True
     return False
 
 
