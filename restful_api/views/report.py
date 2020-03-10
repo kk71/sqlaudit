ecf9a4ff195a2a55b355863ec55e79013d02edd0
fk <kai.fang@kirintech.cn>
2020-03-10 16:26:15 +0800

    fix

--- restful_api/views/report.py
+++ restful_api/views/report.py
@@ -130,7 +130,7 @@ class OnlineReportSchemaRate(PrivilegeReq):
         ret, p = self.paginate(q, **p)
         self.resp([
             i.to_dict(iter_by=lambda k, v: v.values()
-            if v in ("score_rule_type",) else v)
+            if k in ("score_rule_type",) else v)
             for i in ret
         ], **p)
 
