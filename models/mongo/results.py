c0f612b8ed9037d0d9744aa93ee506b787db55ed
fk <kai.fang@kirintech.cn>
2020-03-11 14:29:04 +0800

    fix

--- models/mongo/results.py
+++ models/mongo/results.py
@@ -13,6 +13,8 @@ class Results(BaseDocRecordID):
     schema_name = StringField()
     create_date = DateTimeField()
     etl_date = DateTimeField()
+    capture_time_start = DateTimeField()
+    capture_time_end = DateTimeField()
     ip_address = StringField()
     sid = StringField()
     record_id = StringField()
