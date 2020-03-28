892951ead6a4bf7aab0fe55d8cc7056a087345d1
fk <kai.fang@kirintech.cn>
2020-03-28 11:29:49 +0800

    fix TABLE_NO_INDEX

--- past/rule_analysis/rule/obj/table_no_index.py
+++ past/rule_analysis/rule/obj/table_no_index.py
@@ -5,9 +5,22 @@ def execute_rule(**kwargs):
     username = kwargs.get("username")
     db_cursor = kwargs.get("db_cursor")
     max_score = kwargs.get("max_score")
+    tab_phy_size = kwargs.get("tab_phy_size")
 
-    # sql变量，返回传入用户的组合索引数量和所有索引数量
-    sql = """
+    sql_table_name_bigger_than_threshold = """
+    SELECT segment_name
+  FROM dba_segments
+ WHERE segment_type IN ('TABLE', 'TABLE PARTITION', 'TABLE SUBPARTITION')
+   AND owner = '@username@'
+   AND segment_name NOT LIKE 'BIN%'
+   and bytes / 1024 / 1024 >= @tab_phy_size@
+ ORDER BY 3 DESC
+    """
+    db_cursor.execute(sql_table_name_bigger_than_threshold.
+        replace("@username@", username).replace("@tab_phy_size@", tab_phy_size))
+    big_table_table_names = tuple(db_cursor.fetchall())
+
+    sql = f"""
     SELECT 'COMBINEINDEX',
            COUNT(DISTINCT IC.INDEX_NAME) AS COMBINEINDEXNUMBER
     FROM DBA_IND_COLUMNS IC
@@ -16,19 +29,23 @@ def execute_rule(**kwargs):
       UNION ALL
       SELECT 'ALLINDEX',
              COUNT(1)
-      FROM DBA_INDEXES I WHERE I.OWNER = '@username@'
+      FROM DBA_INDEXES I WHERE I.OWNER = '@username@' 
+      and table_name in {big_table_table_names}
     """
     db_cursor.execute(sql.replace("@username@", username))
     records_comindnum_allinnum = db_cursor.fetchall()
 
+    all_index_sum = 0
     # 取组合索引数量和所有索引数量并赋值给本地变量
     for i in records_comindnum_allinnum:
         if i[0] == 'COMBINEINDEX':
-            comind_num = i[1]
-        if i[0] == 'ALLINDEX':
-            allind_num = i[1]
+            pass
+        elif i[0] == 'ALLINDEX':
+            all_index_sum += i[1]
+        else:
+            pass
 
     # 如果所有索引数量为0，则直接返回，并扣除本规则所有分数。
-    if allind_num == 0:
-        return [], float("%0.2f" % (max_score))
+    if True and all_index_sum == 0:
+        return [], float("%0.2f" % max_score)
     return [], 0.0
