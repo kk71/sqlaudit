a1e330a05e6fb891382a22f8733b9d39b4b8b85d
fk <kai.fang@kirintech.cn>
2020-03-30 10:28:49 +0800

    fix

--- new_rule/ticket-rules/oracle/LONG_TEXT.py
+++ new_rule/ticket-rules/oracle/LONG_TEXT.py
@@ -1,9 +1,6 @@
-import re
-
-
 def code(rule, entries, **kwargs):
     single_sql: dict = kwargs.get("single_sql")
-    sql_text: str = single_sql["sql_text_no_comment"]
+    sql_text: str = single_sql["sql_text"]
 
     if len(sql_text) > rule.gip("char_num"):
         return -rule.weight, []
