a564a7a4fb58eaf7f7809a59a92beae6e048c76b
fk <kai.fang@kirintech.cn>
2020-04-07 17:36:29 +0800

    f

--- oracle_cmdb/cmdb.py
+++ oracle_cmdb/cmdb.py
@@ -4,16 +4,29 @@ __all__ = [
     "OracleCMDB"
 ]
 
-from sqlalchemy import Column, String, Boolean,Integer
+from sqlalchemy import Column, String, Boolean
 
 from cmdb.cmdb import CMDB
+from .plain_db import OraclePlainConnector
 
 
 class OracleCMDB(CMDB):
     """oracle纳管库"""
-    id = Column("id", Integer, primary_key=True)
+
     is_rac = Column("is_rac", Boolean)
     is_pdb = Column("is_pdb", Boolean)
-    service_name = Column("service_name", String)
     sid = Column("sid", String)
+    service_name = Column("service_name", String)
 
+    def build_connector(self, **kwargs):
+        ret_params = [
+            "ip_address", "port", "username", "password"
+        ]
+        if self.sid:
+            ret_params.append("sid")  # 优先考虑用sid去连接纳管库
+        elif self.service_name:
+            ret_params.append("service_name")
+        else:
+            assert 0
+        return OraclePlainConnector(
+            **self.to_dict(iter_if=lambda k, v: k in ret_params))
---
+++ oracle_cmdb/plain_db.py
@@ -0,0 +1,76 @@
+# Author: kk.Fang(fkfkbill@gmail.com)
+
+__all__ = [
+    "OraclePlainConnector"
+]
+
+
+import os
+
+import cx_Oracle
+
+os.environ['NLS_LANG'] = 'SIMPLIFIED CHINESE_CHINA.UTF8'
+
+
+class OraclePlainConnector:
+
+    def __init__(self,
+                 ip_address,
+                 port,
+                 username,
+                 password,
+                 sid=None,
+                 service_name=None):
+        assert sid or service_name
+        self.ip_address = ip_address
+        self.port = port
+        self.username = username
+        self.password = password
+        self.sid = sid
+        self.service_name = service_name
+
+        self.dsn = cx_Oracle.makedsn(self.ip_address, str(self.port), self.sid)
+        self.conn = cx_Oracle.connect(self.username, self.password, self.dsn)
+        self.cursor = self.conn.cursor()
+
+    def insert(self, sql, params=None):
+        params = params or []
+        try:
+            print(sql, params)
+            self.cursor.execute(sql, params)
+        except cx_Oracle.OperationalError as e:
+            print(str(e))
+            self.conn.rollback()
+        self.conn.commit()
+
+    def execute(self, sql, params=None):
+        params = params or []
+        self.cursor.execute(sql, params)
+
+    def update(self, sql, params=None):
+        self.insert(sql, params)
+
+    def select_dict(self, sql, params=None, one=True):
+        params = params or []
+        self.cursor.execute(sql, params)
+        fields = [x[0].lower() for x in self.cursor.description]
+        data = (self.cursor.fetchone() or ()) if one else self.cursor.fetchall()
+
+        data = data or ()
+        if isinstance(data, tuple):
+            return dict(zip(fields, data))
+
+        # Else is a list, fetchall()
+        return [dict(zip(fields, item)) for item in data]
+
+    def select(self, sql, params=None, one=True):
+        params = params or []
+        self.cursor.execute(sql, params)
+        return (self.cursor.fetchone() or ()) if one else self.cursor.fetchall()
+
+    def delete(self, sql, params=None):
+        self.insert(sql, params)
+
+    def close(self):
+        self.cursor.close()
+        self.conn.close()
