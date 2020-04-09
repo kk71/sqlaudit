5c5fe73b5b81b257cc0d651c95b59d21bfc5d848
fk <kai.fang@kirintech.cn>
2020-04-09 14:00:22 +0800

    f

--- startup/migrate.py
+++ startup/migrate.py
@@ -4,6 +4,7 @@
 from utils import migrate_from_oracle as mfo
 
 # new mysql data
+from models.sqlalchemy import *
 from auth.user import *
 from oracle_cmdb.cmdb import *
 from oracle_cmdb.rate import *
