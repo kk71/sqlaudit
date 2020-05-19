9a5d60e11f9cc47fdbab1f813963dee72e45d9a3
fk <kai.fang@kirintech.cn>
2020-05-19 16:59:33 +0800

    apidoc for cmdb list

--- coupling/restful_api/cmdb.py
+++ coupling/restful_api/cmdb.py
@@ -204,6 +204,30 @@ class BaseCMDBHandler(OraclePrivilegeReq):
             CMDBRule.objects.insert(cmdb_rules)
             self.resp_created(new_cmdb.to_dict())
 
+    post.argument = {
+        "json": {
+            "connect_name": "",
+            "group_name": "",
+            "business_name": "",
+            "db_type": "",
+            "server_name": "",
+            "ip_address": "",
+            "port": "",
+            "service_name": "",
+            "username": "",
+            "password": "",
+            "status": 1,
+            "domain_env": "",
+            "is_rac": 1,
+            "db_model": "",
+            "baseline": "0.8",
+            "is_pdb": 1,
+            "version": "",
+            "sid": "",
+            "allow_online": 1,
+        }
+    }
+
     def patch(self):
         """修改CMDB"""
         params = self.get_json_args(Schema({
@@ -264,6 +288,30 @@ class BaseCMDBHandler(OraclePrivilegeReq):
             session.refresh(the_cmdb)
             self.resp_created(the_cmdb.to_dict())
 
+    patch.argument = {
+        "json": {
+            "cmdb_id": 1,
+            "ip_address": "",
+            "port": 1,
+            "service_name": "",
+            "group_name": "",
+            "business_name": "",
+            "db_type": "oracle",
+            "server_name": "",
+            "username": "",
+            "password": "",
+            "status": 1,
+            "domain_env": 1,
+            "is_rac": 1,
+            "db_model": "OLTP",
+            "baseline": 80,
+            "is_pdb": 1,
+            "version": "",
+            "sid": "",
+            "allow_online": 1
+        }
+    }
+
     def delete(self):
         """删除CMDB"""
         params = self.get_json_args(Schema({
@@ -287,6 +335,12 @@ class BaseCMDBHandler(OraclePrivilegeReq):
             cmdb = session.query(CMDB).filter_by(**params).first()
             resp = cmdb.test_connectivity()
             self.resp(resp)
+
+    options.argument = delete.argument = {
+        "json": {
+            "cmdb_id": 1
+        }
+    }
 
 
 @as_view("aggregation", group="cmdb")
@@ -314,3 +368,9 @@ class CMDBAggregationHandler(OraclePrivilegeReq):
                     ret[k].add(qr[i])
             ret = {k: list(v) for k, v in ret.items()}
             self.resp(ret)
+
+    get.argument = {
+        "querystring": {
+            "key": "connect_name,group_name,business_name"
+        }
+    }
