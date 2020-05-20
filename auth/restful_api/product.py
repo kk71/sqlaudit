cd6a84409718ce5b92d64e8e2ba9003ff2682bbf
sqlaudit <574691837@qq.com>
2020-05-20 15:36:52 +0800

    argument

--- auth/restful_api/product.py
+++ auth/restful_api/product.py
@@ -68,6 +68,16 @@ class ProductLicenseHandler(BaseReq):
             print(msg)
             return self.resp_bad_req(msg=str(e))
 
+    get.argument = {
+        "querystring": {},
+        "json": {}
+    }
+    post.argument = {
+        "querystring": {},
+        "json": {
+            "license": "CO4Yvhbu8CWPqsJPGbXqbQ51ZSU+tlrJaSzKXDXM6IoCyGsiXRIMO/WqGmGP2EBKO1ebjBq95YcbbVKSJeE+geR8FLTDZedDVJIrJ1rmTC49b3Yf8oCipBWxx512LKCj3FPSkR/kRYCADtDAp6UuujHcxa8Wxv+4bW8ZCg+bKGVgjjAI1dZ+qm7Y1JCAqh0+cWpDDq/WNz0Z+J8BAcSt/ifDtkuLDaX0GUHk5UqAGzdlHkAyYzRMpxcXsNLFOcwrexU9vvQg+x9Qlnd4O9R17qtFpZPCJ1ah3r0O+eyBftVTvRFQHCM8zqSw3txGd8RTbmg1gVyjaCuiZfDyrSnA9ffu+MmW52/lASgXP4MV+a8gVAAqauVkx0nxL2B42lhoQHYH48vlqr3vvRWV28Cphs2mNn8Xv3rvaCwUyoqX5gpsOwfP8SYQuZawA4MPAj4jouQ9dzv3Nun+vNLOeVie6oslNgFaX3wCSJD5vp9L1NKsh048AwC5OWK1qhvJsDk1qgXQ4Yyzd5iktcqW55owlRMpxiZu6XgDTq9/LZDz84lo/e2yPh4tz+f93jnOGI8mNMrznBj7F1JSq0qVHx5wWd/k/x/OxLaY4+Fs9rKsBk3axCE5+esQU9SJW+vMDSvbY+FUd/wMpdtNqMpFsEuAtNyhjm3n27j/rEhHsUOIHSRgWbzURhbSKrmaDMiVPCZlpGJ4RMxC/CylpJdp1KKR4l3gIjxxGHmn4P70HzGTgUZ1Irh4Cf3fTu5pXfnHsWgJCnP8wc5paDOntoLEKLes/nSAAsqOpL+IYwt1Dxdl+cn/tUoqqdLXNrusYZ4ct37VPPshqGl2TPUydAVwwcC2N5mG8fMKfmGl9mFmMe6DX2+cIZI9sWPcaKQotqsP7S6G8js+kvgQlPHlEwOt7xQN1sqDqQB8LM+4YqC/FTws5ZanTgvgFQZ2/1wfPfo9PAWYmFnI9rV5UKchn59XxolHR0B8l21nFX+w6YzDZKOs5RlZWYVBb6ivkrSGbgAlOorRJU4LujHEt+U04c3KXWS+YiaQl4h/DqKM8EBsbcIUr6R9/cp9clVuVzhyYqtkEPybAPs6qMhzCS7EDr6ZSjag/O2gI+nBGagQ2vBVrUmvF4ONoLYSkmUWC9kumYTswSFOZmdyH6mhY8PWjYJUMpUv4EtdkFYmwzj+nIn1vpXnUqQcZsjI5Peh/+SJML1CiA0QXar290rffB0G7U75c7jRQr8PrMULTQlfI8ZVYgF0iCSvRgj1Yc5eiv10s6xMHVSx84KUsS3x+Vh+9tw3PIZ5C1HFpcdh0aC+Eh68Py1gs6X1suCBs8KR/fpUkyqmkPcX4t33O2shZtzzNwwjSVdV/g==",
+        }
+    }
 
 @as_view("version", group="version")
 class VersionHandler(BaseReq):
@@ -83,3 +93,11 @@ class VersionHandler(BaseReq):
         rst["versions"].reverse()
         rst["versions"], p = self.paginate(rst["versions"], **p)
         self.resp(dt_to_str(rst), **p)
+
+    get.argument = {
+        "querystring": {
+            "page": "1",
+            "per_page": "10"
+        },
+        "json": {}
+    }
