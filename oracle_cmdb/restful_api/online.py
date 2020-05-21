e964733a1339aa7705f4ef82b77909158d866e8c
sqlaudit <574691837@qq.com>
2020-05-21 09:51:34 +0800

    online

--- oracle_cmdb/restful_api/online.py
+++ oracle_cmdb/restful_api/online.py
@@ -10,7 +10,13 @@ from ..tasks.capture.cmdb_task_capture import OracleCMDBTaskCapture
 from ..statistics.current_task.phy_size import OracleStatsCMDBPhySize
 from ..statistics.login_user_current_rank.sql_execution_cost_rank import OracleStatsCMDBSQLExecutionCostRank
 from ..statistics.current_task.risk_rule import OracleStatsSchemaRiskRule
+from ..capture.obj_tab_info import OracleObjTabInfo
 from ..capture.obj_tab_space import OracleObjTabSpace
+from ..capture.obj_tab_col import OracleObjTabCol
+from ..capture.obj_part_tab_parent import OracleObjPartTabParent
+from ..capture.obj_ind_col_info import OracleObjIndColInfo
+from ..capture.obj_view_info import OracleObjViewInfo
+
 
 
 @as_view("overview", group="online")
@@ -183,3 +189,116 @@ class TablespaceTotalHistoryHandler(OraclePrivilegeReq):
         },
         "json": {}
     }
+
+
+@as_view("metadata",group="online")
+class MetadataListHandler(OraclePrivilegeReq):
+
+    def get(self):
+        """元数据查询"""
+
+        self.acquire(PRIVILEGE.PRIVILEGE_METADATA)
+
+        params = self.get_query_args(Schema({
+            "cmdb_id": scm_int,
+            "schema_name": scm_unempty_str,
+            scm_optional("table_name"): scm_unempty_str,
+            scm_optional("search_type", default="exact"): And(
+                scm_str, scm_one_of_choices(("icontains", "exact"))),
+            **self.gen_p()
+        }))
+        cmdb_id=params.pop('cmdb_id')
+        search_type = params.pop('search_type')
+        if params.get("table_name", None):
+            if "." in params["table_name"]:
+                print(f"warning: the original table_name is {params['table_name']} "
+                      f"the word before the dot is recognized as schema and has been ignored.")
+                params["table_name"] = params["table_name"].split(".")[1]
+            params[f"table_name__{search_type}"] = params.pop('table_name')
+        p = self.pop_p(params)
+
+        with make_session() as session:
+            latest_task_record = OracleCMDBTaskCapture.last_success_task_record_id_dict(session, cmdb_id)
+            latest_task_record_id = latest_task_record.get(cmdb_id, None)
+            if not latest_task_record:
+                return self.resp_bad_req(msg=f"当前库未采集或者没有采集成功。")
+
+        tabinfo_q=OracleObjTabInfo.objects(cmdb_id=cmdb_id,
+                                  task_record_id=latest_task_record_id).\
+            filter(**params).order_by('-create_time')
+
+        items, p = self.paginate(tabinfo_q, **p)
+        self.resp([i.to_dict() for i in items], **p)
+
+    get.argument = {
+        "querystring": {
+            "cmdb_id": "2526",
+            "schema_name": "APEX",
+            "//table_name": "PY_GVSQL",
+            "//search_type": "icontains",
+            "page": "1",
+            "per_page": "10"
+        },
+        "json": {}
+    }
+
+@as_view("tableinfo",group="online")
+class TableInfoHandler(OraclePrivilegeReq):
+
+    def get(self):
+        """表、表字段、表分区、表索引、表视图信息,库schema最新采集"""
+        params = self.get_query_args(Schema({
+            "cmdb_id": scm_int,
+            "schema_name": scm_unempty_str,
+            "table_name": scm_unempty_str
+        }))
+        if params.get("table_name", None) and "." in params["table_name"]:
+            print(f"warning: the original table_name is {params['table_name']} "
+                  f"the word before the dot is recognized as schema and has been ignored.")
+            params["table_name"] = params["table_name"].split(".")[1]
+
+        latest_tabinfo=OracleObjTabInfo.objects(table_name=params["table_name"]).\
+            order_by("-create_time").first()
+        if not latest_tabinfo:
+            self.resp({}, msg="无数据。")
+            return
+        params["task_record_id"] = latest_tabinfo.task_record_id
+
+        self.resp({
+            'table': [i.to_dict(iter_if=lambda k, v: k in (
+                "schema_name", "table_name", "table_type", "iot_name", "num_rows",
+                "blocks", "avg_row_len", "last_analyzed", "last_ddl_time",
+                "chain_cnt", "hwm_stat", "compression", "phy_size_mb"
+            )) for i in OracleObjTabInfo.objects(**params)],
+
+            'field': [i.to_dict(iter_if=lambda k, v: k in (
+                "schema_name", "table_name", "column_name", "data_type", "nullable",
+                "num_nulls", "num_distinct", "data_default", "avg_col_len"
+            )) for i in OracleObjTabCol.objects(**params)],
+
+            'partition': [i.to_dict(iter_if=lambda k, v: k in (
+                "schema_name", "table_name", "partitioning_type", "column_name",
+                "partitioning_key_count", "partition_count",
+                "sub_partitioning_key_count", "sub_partitioning_type",
+                "last_ddl_time", "phy_size_mb"
+            )) for i in OracleObjPartTabParent.objects(**params)],# "num_rows"TODO
+
+            'index': [i.to_dict(iter_if=lambda k, v: k in (
+                "schema_name", "index_name", "table_owner", "table_name", "column_name",
+                "column_position", "descend"
+            )) for i in OracleObjIndColInfo.objects(**params)],
+            'view': [i.to_dict(iter_if=lambda k, v: k in (
+                "obj_pk", "schema_name", "view_name", "referenced_owner", "referenced_name",
+                "referenced_type"
+            )) for i in OracleObjViewInfo.objects(**params)]
+        })
+
+    get.argument = {
+        "querystring": {
+            "cmdb_id": 2526,
+            "schema_name": "IDBAAS",
+            "table_name": "T_CMDB_ORACLE"
+        },
+        "json": {}
+    }
+
