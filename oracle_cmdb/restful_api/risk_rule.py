081ad26a0210e354fb704eec39ecae077c0a996a
sqlaudit <574691837@qq.com>
2020-05-25 15:13:45 +0800

    riskrule_sql

--- oracle_cmdb/restful_api/risk_rule.py
+++ oracle_cmdb/restful_api/risk_rule.py
@@ -3,7 +3,7 @@ from typing import Union
 from .base import OraclePrivilegeReq
 from ..const import *
 from ..issue.sql import OracleOnlineSQLIssue
-from ..capture.sqlstat import OracleSQLStatToday,OracleSQLStat
+from ..capture.sqlstat import OracleSQLStatToday, OracleSQLStat
 from ..const import ONLINE_RISK_RULE_ENTRIES
 from ..statistics.current_task.risk_rule import OracleStatsSchemaRiskRule
 from ..tasks.capture import OracleCMDBTaskCapture
@@ -13,8 +13,7 @@ from models.sqlalchemy import make_session
 from restful_api.modules import as_view
 
 
-
-@as_view("riskrule",group="online")
+@as_view("riskrule", group="online")
 class RiskRuleHandler(OraclePrivilegeReq):
 
     def get(self):
@@ -82,7 +81,7 @@ class RiskRuleHandler(OraclePrivilegeReq):
     }
 
 
-@as_view("riskrule_sql",group="online")
+@as_view("riskrule_sql", group="online")
 class RiskRuleSQLHandler(OraclePrivilegeReq):
 
     def get(self):
@@ -91,9 +90,10 @@ class RiskRuleSQLHandler(OraclePrivilegeReq):
         (触犯一个规则的一个sql会出现多个相同sql因为一个sql_id有多个plan_hash_value),
         entry=SQL,
         平均时间和执行次数为today当前today采集,
+        (注意:sql_id下有的plan_hash_value并没有执行特征所有会有为None的)
         执行次数直接来自采集execution_total 字段。
         平均时间直接来自采集elapsed_time_total/execution_total。"""
-        params=self.get_query_args(Schema({
+        params = self.get_query_args(Schema({
             "cmdb_id": scm_int,
             "schema_name": scm_str,
             "rule_name": scm_str,
@@ -102,30 +102,30 @@ class RiskRuleSQLHandler(OraclePrivilegeReq):
             **self.gen_p()
         }))
         entry = params.pop("entry")
-        p=self.pop_p(params)
+        p = self.pop_p(params)
 
-        #sql_issue_q:会出现相同的sql_id,因为触犯一个规则一个sql_id一个plan_hash_value为一个文档
-        # (若要只拿一个sql_id那么拿哪个plan_hash_value的)
-        sql_issue_q=OracleOnlineSQLIssue.objects(**params).filter(OracleOnlineSQLIssue.entries==entry)
+        sql_issue_q = OracleOnlineSQLIssue.objects(**params).filter(OracleOnlineSQLIssue.entries == entry)
 
-        _=params.pop("rule_name")
-        today_sqlstat_q = OracleSQLStat.objects(two_days_capture=SQL_TWO_DAYS_CAPTURE_TODAY).filter(**params)
-        sql_issue=[]
+        _ = params.pop("rule_name")
+        today_sqlstat_q = OracleSQLStatToday.filter(**params)
+        sql_issue = []
         for sql_issue_q_o in sql_issue_q:
-            sql_issue_o=sql_issue_q_o.to_dict()
+            sql_issue_o = sql_issue_q_o.to_dict()
             for today_sqlstat_q_o in today_sqlstat_q:
                 if sql_issue_o['output_params']['sql_id'] == today_sqlstat_q_o.sql_id \
                         and sql_issue_o['output_params']['plan_hash_value'] == today_sqlstat_q_o.plan_hash_value:
-                    sql_issue_o['executions_total']=today_sqlstat_q_o.executions_total
+                    sql_issue_o['executions_total'] = today_sqlstat_q_o.executions_total
                     if sql_issue_o['executions_total']:
-                        sql_issue_o['execution_time_cost_on_average']=round(round(today_sqlstat_q_o.elapsed_time_total,2)/sql_issue_o['executions_total'],2)
-                else:#?为啥会没有呀
-                    sql_issue_o['executions_total']=0
-                    sql_issue_o['execution_time_cost_on_average']=0
+                        sql_issue_o['execution_time_cost_on_average'] = round(
+                            round(today_sqlstat_q_o.elapsed_time_total, 2) / sql_issue_o['executions_total'], 2)
+                else:  # 考虑到触犯规则记录了每一个plan_hash_value维度，一个sql多个plan_hash_value，真正产生执行特征的是其中一个plan_hash_value或几个
+                    sql_issue_o['executions_total'] = None
+                    sql_issue_o['execution_time_cost_on_average'] = None
 
             sql_issue.append(sql_issue_o)
-        sql_issue,p=self.paginate(sql_issue,**p)
-        self.resp(sql_issue,**p)
+        sql_issue, p = self.paginate(sql_issue, **p)
+        self.resp(sql_issue, **p)
+
     get.argument = {
         "querystring": {
             "cmdb_id": "2526",
@@ -135,4 +135,3 @@ class RiskRuleSQLHandler(OraclePrivilegeReq):
             "entry": "SQL"
         }
     }
-
