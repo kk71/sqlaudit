3b3126043364d520f5f09c317d185749b1a61004
sqlaudit <574691837@qq.com>
2020-06-03 11:56:00 +0800

    risk_sql_obj

--- oracle_cmdb/restful_api/risk_rule.py
+++ oracle_cmdb/restful_api/risk_rule.py
@@ -1,12 +1,11 @@
 from typing import Union
 
 from .base import OraclePrivilegeReq
-from ..issue.sql import OracleOnlineSQLIssue
-from ..capture.sqlstat import OracleSQLStatToday
+from ..statistics.current_task.risk_sql import OracleStatsSchemaRiskSQL
 from ..statistics.current_task.risk_rule import OracleStatsSchemaRiskRule
 from ..tasks.capture import OracleCMDBTaskCapture
 from utils.schema_utils import *
-from rule.const import ALL_RULE_LEVELS,RULE_ENTRY_ONLINE_SQL
+from rule.const import ALL_RULE_LEVELS
 from models.sqlalchemy import make_session
 from restful_api.modules import as_view
 
@@ -83,56 +82,57 @@ class RiskRuleHandler(OraclePrivilegeReq):
 class RiskRuleSQLHandler(OraclePrivilegeReq):
 
     def get(self):
-        """触犯规则的sql,
-        库一次采集一个schema触犯一个规则的sql
-        (触犯一个规则的一个sql会出现多个相同sql因为一个sql_id有多个plan_hash_value),
-        entry=SQL,
-        平均时间和执行次数为today当前today采集,
-        (注意:sql_id下有的plan_hash_value并没有执行特征所有会有为None的,
-        平均时间为0证明:短时间内执行很多很多次结果保留3为小数目前)
-        执行次数直接来自采集execution_total 字段。
-        平均时间直接来自采集elapsed_time_total/execution_total。"""
+        """触犯风险sql规则的sql,
+        库一次采集一个schema触犯一个规则的sql,
+        (违反数量为sql_id去重结果(出现多同一sql_id，因一个sql_id有多plan_hash_value))
+        """
         params = self.get_query_args(Schema({
             "cmdb_id": scm_int,
             "schema_name": scm_str,
             "rule_name": scm_str,
             "task_record_id": scm_int,
-            "entry": scm_one_of_choices(RULE_ENTRY_ONLINE_SQL),
             **self.gen_p()
         }))
-        entry = params.pop("entry")
         p = self.pop_p(params)
 
-        sql_issue_q = OracleOnlineSQLIssue.objects(**params).filter(entries__in=list(entry))
-
-        _ = params.pop("rule_name")
-        today_sqlstat_q = OracleSQLStatToday.filter(**params)
-        sql_issue = []
-        for sql_issue_q_o in sql_issue_q:
-            sql_issue_o = sql_issue_q_o.to_dict()
-
-            # 考虑到触犯规则记录了每一个plan_hash_value维度，一个sql多个plan_hash_value，真正产生执行特征的是其中一个plan_hash_value或几个
-            sql_issue_o['executions_total'] = None
-            sql_issue_o['execution_time_cost_on_average'] = None
-
-            for today_sqlstat_q_o in today_sqlstat_q:
-                if sql_issue_o['output_params']['sql_id'] == today_sqlstat_q_o.sql_id \
-                        and sql_issue_o['output_params']['plan_hash_value'] == today_sqlstat_q_o.plan_hash_value:
-                    sql_issue_o['executions_total'] = today_sqlstat_q_o.executions_total
-                    if sql_issue_o['executions_total']:
-                        sql_issue_o['execution_time_cost_on_average'] = round(
-                            round(today_sqlstat_q_o.elapsed_time_total, 2) / sql_issue_o['executions_total'], 3)
-            sql_issue.append(sql_issue_o)
-
-        sql_issue, p = self.paginate(sql_issue, **p)
-        self.resp(sql_issue, **p)
+        risk_sql_q=OracleStatsSchemaRiskSQL.filter(**params)
+        risk_sql_q, p= self.paginate(risk_sql_q,**p)
+        self.resp([risk_sql.to_dict() for risk_sql in risk_sql_q],**p)
 
     get.argument = {
         "querystring": {
             "cmdb_id": "2526",
             "schema_name": "ISQLAUDIT_DEV",
-            "rule_name": "SQL_LOOP_NUM",
-            "task_record_id": "39",
-            "entry": "SQL"
+            "rule_name": "CHECK_LOB_USING",
+            "task_record_id": "54",
+        }
+    }
+
+
+@as_view("obj", group="online")
+class RiskRuleOBJHandler(OraclePrivilegeReq):
+
+    def get(self):
+        """触犯风险obj规则的obj"""
+        params = self.get_query_args(Schema({
+            "cmdb_id": scm_int,
+            "schema_name": scm_str,
+            "rule_name": scm_str,
+            "task_record_id": scm_int,
+            **self.gen_p()
+        }))
+        p = self.pop_p(params)
+        from ..statistics.current_task.risk_object import OracleStatsSchemaRiskObject
+
+        risk_obj_q=OracleStatsSchemaRiskObject.filter(**params)
+        risk_obj_q,p=self.paginate(risk_obj_q,**p)
+        self.resp([risk_obj.to_dict() for risk_obj in risk_obj_q],**p)
+
+    get.argument = {
+        "querystring": {
+            "cmdb_id": "2526",
+            "schema_name": "DVSYS",
+            "rule_name": "USE_FUNCTION",
+            "task_record_id": "56"
         }
     }
