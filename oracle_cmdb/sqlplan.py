74912d120d0a2f50be1b33761336a8897656786f
fk <kai.fang@kirintech.cn>
2020-06-12 14:47:03 +0800

    f

--- oracle_cmdb/sqlplan.py
+++ oracle_cmdb/sqlplan.py
@@ -36,12 +36,20 @@ class BaseOracleSQLPlanCommon:
         pt.align = "l"
         for plan in plans.values_list(*cls.ORACLE_PLAN_HEAD.values()):
             to_add = [i if i is not None else " " for i in plan]
+            for i in range(len(to_add)):
+                try:
+                    to_add[i] = int(to_add[i])
+                except ValueError:
+                    try:
+                        to_add[i] = float(to_add[i])
+                    except Exception as e:
+                        pass
             m, s = divmod(to_add[-1] if to_add[-1] and to_add[-1] != " " else 0, 60)
             h, m = divmod(m, 60)
             to_add[-1] = "%02d:%02d:%02d" % (h, m, s)
-            for i in range(3, 6):  # convert to int if range 3~5 is not None
-                if to_add[i] and to_add[i].strip():
-                    to_add[i] = int(to_add[i])
+            # for i in range(3, 6):  # convert to int if range 3~5 is not None
+            #     if to_add[i] and to_add[i] to_add[i].strip():
+            #         to_add[i] = int(to_add[i])
             if 8 > len(str(to_add[3])) > 5:
                 to_add[3] = str(round(to_add[3] // 1024)) + "K"
                 if len(str(to_add[3])) >= 8:
